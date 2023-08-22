d2941edb6dac352de1e5040e520ce30ab14755d3
ElianaMT <eliana_taco@estudante.sesisenai.org.br>
2023-08-22 12:00:41 -0500

    Ex 5 - Criação de link para a página de Formulário de cadastro de medicamento

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,22 @@
 <template>
   <Header />
-  
-  
-  <div class="container1">
-
-    <CardMedicamento v-if="!!listaMedicamentos" 
-    v-for="medicamento in listaMedicamentos" 
-    :key="medicamento.id"
-      @favoritar="FavoritarMedicamento" 
-      :nome="medicamento.nome" 
-      :laboratorio="medicamento.laboratorio"
-      :preco="medicamento.preco" 
-      :id="medicamento.id" />
-
-  </div>
 
   <router-view></router-view>
 </template>
   
 <script >
 import Header from "./components/Header.vue"
-import CardMedicamento from "./components/CardMedicamento/index.vue"
+
 
 export default {
   components: {
-    Header,
-        CardMedicamento
+    Header
   },
-
-  data() {
-    return {
-      listaMedicamentos: []
-    }
-  },
-
-  methods: {  
-
-    FavoritarMedicamento(id) {
-
-      this.listaMedicamentos = this.listaMedicamentos.map(item => {
-
-        if (item.id === id) {
-          item.favorito = !item.favorito
-        }
-        return item
-      })
-    }
-  }
 }
 
 </script>
   
 <style scoped>
-.container1 {
-  display: flex;
-  flex-wrap: wrap;
-  widows: 100vw;
-  padding: 1em;
-}
+
 </style>
   
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,57 @@
 <template>
+<div class="ma-5 d-flex justify-end">
+<router-link to="/novo-medicamento">
+    <v-btn title="Adicionar mais medicamentos" variant="outlined" color="success">Cadastrar Medicamentos</v-btn>
+</router-link>
+</div>
+
     <div>
-        Home
-    </div>
+<CardMedicamento v-if="!!listaMedicamentos" 
+v-for="medicamento in listaMedicamentos" 
+:key="medicamento.id"
+  @favoritar="FavoritarMedicamento" 
+  :nome="medicamento.nome" 
+  :laboratorio="medicamento.laboratorio"
+  :preco="medicamento.preco" 
+  :id="medicamento.id" />
+
+</div>
+   
 </template>
+
+<script>
+import CardMedicamento from "../components/CardMedicamento/index.vue"
+export default{ 
+    components: { 
+        CardMedicamento
+    },
+    data() {
+    return {
+      listaMedicamentos: [
+        { 
+            id:1,
+            nome: "Paracetamol",
+            laboratorio:"Clamed",
+            preco:20,
+            favorito:false
+      }
+      ]
+    }
+  },
+
+  methods: {  
+
+    FavoritarMedicamento(id) {
+
+      this.listaMedicamentos = this.listaMedicamentos.map(item => {
+
+        if (item.id === id) {
+          item.favorito = !item.favorito
+        }
+        return item
+      })
+    }
+  }
+}
+</script>
+
